379c51a52fa17e60b84ac1f034dbd322da3505fc
Pedr0P <pedrovbpereira10@gmail.com>
2026-09-08 15:26:36 -0300

    Blink: Update shortcut

--- lua/core/blink.lua
+++ lua/core/blink.lua
@@ -10,7 +10,7 @@ cmp.setup({
 		["<C-h>"] = { "hide", "fallback" },
 		["<C-n>"] = { "select_next", "fallback" },
 		["<C-e>"] = { "select_prev", "fallback" },
-		["<C-i>"] = { "select_and_accept", "fallback" },
+		["<C-l>"] = { "select_and_accept", "fallback" },
 
 		["<C-f>"] = { "scroll_documentation_up", "fallback" },
 		["<C-b>"] = { "scroll_documentation_down", "fallback" },
@@ -18,7 +18,7 @@ cmp.setup({
 		["<Tab>"] = { "snippet_forward", "fallback" },
 		["<S-Tab>"] = { "snippet_backward", "fallback" },
 
-		["<C-l>"] = { "show_signature", "hide_signature", "fallback" },
+		["<C-s>"] = { "show_signature", "hide_signature", "fallback" },
 	},
 	cmdline = {
 		keymap = { preset = "inherit", },
